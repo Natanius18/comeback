f332f4888f2359998f3a157cec35cd53afd6dbd2
Natan <59052957+Natanius18@users.noreply.github.com>
2021-02-12 15:46:30 +0200

    Top categories

--- src/main/java/CSVReader.java
+++ src/main/java/CSVReader.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class CSVReader {
-    public static void importFromCSV() {
+    public static ArrayList<Product> importFromCSV() {
         ArrayList<Product> products = new ArrayList<>();
         String csvFile = "C:\\Users\\Натан\\Desktop\\Products.csv";
         String line;
@@ -11,15 +11,16 @@ public class CSVReader {
             while ((line = br.readLine()) != null) {
                 products.add(parseContactLine(line));
             }
-            for (Product product : products) {
-                System.out.println("Name: " + product.getName());
-                System.out.println("Price: " + product.getPrice());
-                System.out.println("Category: " + product.getCategory().getName());
-                System.out.println("--------------------------");
-            }
+//            for (Product product : products) {
+//                System.out.println("Name: " + product.getName());
+//                System.out.println("Price: " + product.getPrice());
+//                System.out.println("Category: " + product.getCategory().getName());
+//                System.out.println("--------------------------");
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return products;
     }
 
     private static Product parseContactLine(String line) {
@@ -27,4 +28,3 @@ public class CSVReader {
         return new Product(data[0], Integer.valueOf(data[1]));
     }
 }
-
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        CSVReader.importFromCSV();
+        CSVReader.importFromCSV().stream()
+                .collect(Collectors.groupingBy(p -> p.getCategory().getName(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(entry -> System.out.println("Category: " + entry.getKey() + ", total: " + entry.getValue()));
     }
 }
